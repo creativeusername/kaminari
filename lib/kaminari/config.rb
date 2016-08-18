7e70f548f8165b41730aa8b139d3c9da4567e78a
Max Kerp <maxkerp@gmail.com>
2016-08-18 10:14:37 +0200

    Use default values for config_accessor
    Hi,
    I came across this file and stumbled upon your comment. A look in the AS::Configurable module revealed a way to set default values for config_accessor. I thought i just share this with you. This is not tested (I did not even clone the repo). This PR is to be considered as a hint, nothing more. Since  I don't understand the code that well, I did not know how to make `param_name` have a default value.
    Hope this helps :)

--- lib/kaminari/config.rb
+++ lib/kaminari/config.rb
@@ -17,15 +17,20 @@ module Kaminari
   # need a Class for 3.0
   class Configuration #:nodoc:
     include ActiveSupport::Configurable
-    config_accessor :default_per_page
-    config_accessor :max_per_page
-    config_accessor :window
-    config_accessor :outer_window
-    config_accessor :left
-    config_accessor :right
-    config_accessor :page_method_name
-    config_accessor :max_pages
-    config_accessor :params_on_first_page
+    config_accessor :default_per_page { 25 }
+    config_accessor :max_per_page { nil }
+    config_accessor :window { 4 }
+    config_accessor :outer_window { 0 }
+    config_accessor :left { 0 }
+    config_accessor :right { 0 }
+    config_accessor :page_method_name { :page }
+    config_accessor :max_pages { nil }
+    config_accessor :params_on_first_page { false }
+    
+    # TODO Set default value for param_name
+    # I do not realy understand why param_name is not
+    # a config_accessor
+    config.param_name = :page
 
     def param_name
       config.param_name.respond_to?(:call) ? config.param_name.call : config.param_name
@@ -35,19 +40,5 @@ module Kaminari
     writer, line = 'def param_name=(value); config.param_name = value; end', __LINE__
     singleton_class.class_eval writer, __FILE__, line
     class_eval writer, __FILE__, line
-  end
-
-  # this is ugly. why can't we pass the default value to config_accessor...?
-  configure do |config|
-    config.default_per_page = 25
-    config.max_per_page = nil
-    config.window = 4
-    config.outer_window = 0
-    config.left = 0
-    config.right = 0
-    config.page_method_name = :page
-    config.param_name = :page
-    config.max_pages = nil
-    config.params_on_first_page = false
   end
 end
